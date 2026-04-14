3402d9ca6b8ea21bad0577a0a3fe7046f98a71fd
INJ Note <nachojkn.codes@gmail.com>
2026-04-14 19:11:22 -0300

    Se agregan notas de clase 14-4-2026

--- practica1_Objetos/p1Obj.java
+++ practica1_Objetos/p1Obj.java
@@ -16,6 +16,25 @@ public class p1Obj {
     * Cuando se crea una instancia de una clase, JAVA le asigna una direccion de memoria dentro de la memoria estatica al nombre del objeto, y en la memoria dinamica se le asigna  un espacio para guardar su estado (características del objeto).
     * En el nombre de la instancia creada se coloca una referencia al objeto
     *
+    * CONSTRUCTORES
+    * Cuando se usa el comando "new", el sistema llama a un metodo especial llamado constructor, que crea el objeto
+    *
+    * ALIASING --> PREGUNTA DE PARCIAL --> Se le llama aliasing a la situacion donde dos variables referencian a un mismo objeto
+    *
+    * PREGUNTA DE PARCIAL --> Cual es la diferencia entre comparar variables con "==" y ".equals()"??
+    *   --> == Compara direcciones de memoria; .equals() compara los valores dentro de estas variables
+    * Caso particular, en una situacion de aliasing p1 == p2 arrojaría true (si estas son las variables que apuntan a un mismo objeto)
+    *
+    *
+    *
+    * Cuando declaramos una variable sin valores, por defecto se le asigna null a esas variables dentro del objeto.
+    * si creamos "Point p1;" o "Point p2 = null;" es lo mismo, ambos seran null. Si buscamos acceder a alguna propiedad de alguno de esos objetos la consola arrojará "NullPointerException"
+    *
+    * Para borrar la referencia a un objeto se le puede asignar null; por ejemplo existe el Point "p1" y le asigno p1 = null;
+    *  --> esto provoca que pierda sus propiedades, y el objeto queda sin referencia
+    *  --> En este caso, se puede pasar el garbage collector para borrar de la memoria dinámica de todos aquellos objetos sin referencia
+    *
+    *
     * */
 
     static double distancia(Point p1, Point p2) {
